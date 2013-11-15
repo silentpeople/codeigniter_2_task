3ba68075070a0904a5ff3a166ea9bfd806ef891e
silentpeople <silentpeople@users.noreply.github.com>
2013-11-15 11:32:59 +0800

    Create home_view.php

---
+++ views/home_view.php
@@ -0,0 +1,12 @@
+<html>
+	 <head>
+	   <title>Task2</title>
+	 </head>
+	 <body>
+	   <h1>Home</h1>
+	   <h2>Welcome <?php echo $username; ?>!</h2>
+<br><br>
+           
+	   <a href="home/logout">Logout</a>
+	 </body>
+	</html>
