fee187d8a8d04ad856ad41586b2147aee761dcf6
silentpeople <silentpeople@users.noreply.github.com>
2013-11-15 11:32:08 +0800

    Create login_view.php

---
+++ views/login_view.php
@@ -0,0 +1,20 @@
+
+
+<html>
+	 <head>
+	   <title>task2</title>
+	 </head>
+	 <body>
+	   <h1>Login</h1>
+	   <?php echo validation_errors(); ?>
+	   <?php echo form_open('verifylogin'); ?>
+	     <label for="username">Username:</label>
+	     <input type="text" size="20" id="username" name="username"/>
+	     <br/>
+	     <label for="password">Password:</label>
+	     <input type="password" size="20" id="passowrd" name="password"/>
+	     <br/>
+	     <input type="submit" value="Login"/>
+	   </form>
+	 </body>
+	</html>
