0b63f6be8ddae48e90ff35af5d0c985847c0cb12
silentpeople <silentpeople@users.noreply.github.com>
2013-11-15 11:28:27 +0800

    Create login.php

---
+++ controller/login.php
@@ -0,0 +1,25 @@
+
+
+
+
+<?php 
+
+//for login process
+    if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	 
+	class Login extends CI_Controller {
+	 
+	 function __construct()
+	 {
+	   parent::__construct();
+	 }
+	 
+	 function index()
+	 {
+	   $this->load->helper(array('form'));
+	   $this->load->view('login_view');
+	 }
+	 
+	}
+	 
+	?>
